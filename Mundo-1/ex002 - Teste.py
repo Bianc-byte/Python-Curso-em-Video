e4aebec03dd924bd223db0efe7e1751eadfb0317
Bianca Bueno <biancacunha326@gmail.com>
2025-07-11 23:02:10 -0300

    Atualizando

---
+++ Mundo-1/ex002 - Teste.py
@@ -0,0 +1,12 @@
+name = input('Digite o seu nome: ')
+mother = input('Digite o nome da sua mãe: ')
+print('Olá é um prazer te conhecer, {}!'.format(name))
+print('Eu queria muito conhecer o filho da', mother, ', ela é incrivél')
+
+print('Agora bora aprender matemática para ser digno dela')
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma entre {} e {} vale: {}, agora quero ver você calculando sozinho!!'.format(n1,n2,soma))
+#posso fazer assim tbm
+# print('A soma entre', n1, 'e', n2, 'vale: ', soma, ', agora quero ver você calculando sozinho!!')
